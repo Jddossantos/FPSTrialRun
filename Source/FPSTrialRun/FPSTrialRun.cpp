b303d82baf3244bf3e66ab8c684a87ab3da9aa23
Blu3Dragon01 <joshuaddossantos@gmail.com>
2024-11-22 19:46:40 -0500

    Project Start

---
+++ Source/FPSTrialRun/FPSTrialRun.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FPSTrialRun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPSTrialRun, "FPSTrialRun" );
